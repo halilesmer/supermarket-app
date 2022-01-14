e0a7242a396e278129ee8914b7eaf259aeb841d1
halilesmer <taximinator@gmail.com>
2022-01-14 02:29:39 +0100

    add product

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar.js";
 import Home from "./Components/Home.js";
@@ -10,12 +10,32 @@ import Cart from "./Components/Cart.js";
 function App() {
   const [cart, setCart] = useState([]);
 
+useEffect(() => {
+  // to visualize the cart in the console every time in changes
+  // you can also use React dev tools
+  console.log("cart", cart);
+}, [cart]);
+  
+  
   function handleProductDelete(id) {
     console.log("Deleting product " + id);
   }
 
   function handleProductAdd(newProduct) {
     console.log("Adding product " + newProduct.id);
+    const existingProduct = cart.find(product => product.id === newProduct.id)
+
+    if (existingProduct) {
+      const updateCart = cart.map(product => {
+        if (product.id === newProduct.id) {
+          return {...product, quantity: product.quantity +1}
+        }
+        return product;
+      })
+       setCart(updateCart);
+    } else {
+      return setCart([...cart, {...newProduct, quantity: 1}])
+    }
   }
 
   return (
